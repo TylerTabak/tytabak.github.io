f44d988aaf7f5f59b7e5bd6d2b949044e882be59
TylerTabak <tyler.t@web3.ca>
2021-01-19 21:11:39 -0700

    SVG carousel
    Animated carousel of stack

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ function update(e){
   
   if (window.screen.width > 1000 ){
   document.addEventListener('mousemove',update)}
+
+  var imgArray = [
+    'images/javascript.svg',
+    'images/vue.svg',
+    'images/laravel.svg',
+    'images/python.svg',
+    'images/java.svg',
+    'images/wordpress.svg',
+    'images/php.svg',
+    'images/mongodb.svg',
+    'images/mysql.svg'],
+    curIndex = 0;
+    imgDuration = 3000;
+
+function slideShow() {
+    var x = document.getElementById('slider');
+    x.className += "fadeOut";
+    setTimeout(function() {
+        x.src = imgArray[curIndex];
+        x.className = "";
+        console.log(curIndex);
+        console.log(x.src);
+    },1000);
+    curIndex++;
+    if (curIndex == imgArray.length) { curIndex = 0; }
+    setTimeout(slideShow, imgDuration);
+}
