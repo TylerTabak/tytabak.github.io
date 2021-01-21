b7fcba8a3720007722d4f5fc44ee7791dc854d18
TylerTabak <tyler.t@web3.ca>
2021-01-21 10:54:11 -0700

    Added Projects
    Added projects button with animation and links to github

--- script.js
+++ script.js
@@ -6,6 +6,24 @@ function update(e){
     document.documentElement.style.setProperty('--cursorY', y + 'px')
   }
   
+
+function openProjects() {
+    var y = document.getElementById('project-btn');
+    var z = document.getElementById('projects-hdn');
+    var w = document.getElementById('project-toggle');
+    if (!y.classList.contains('open')) {
+        y.classList.add('open');
+        z.classList.remove('hidden');
+        w.innerHTML = "Projects";
+        setTimeout(function(){ z.classList.remove('invisible'); }, 2500);
+    } else{
+        y.classList.remove('open');
+        z.classList.add('hidden');
+        z.classList.add('invisible');
+        w.innerHTML = `<i id="project-icon" class="fas fa-lightbulb"></i>`
+    }
+}
+
   if (window.screen.width > 1000 ){
   document.addEventListener('mousemove',update)}
 
