3b0927639f12e2587cb9938e07796337ce631684
TylerTabak <tyler.t@web3.ca>
2021-02-17 10:17:50 -0700

    New Project Image

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function update(e){
     var x = e.clientX 
-    var y = e.clientY 
-  
+    var y = e.clientY
     document.documentElement.style.setProperty('--cursorX', x + 'px')
     document.documentElement.style.setProperty('--cursorY', y + 'px')
   }
